5fc66f6805c5844f76ea17cb68118da45cc439ad
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-30 14:10:12 -0700

    created model for member

---
+++ app/models/member.rb
@@ -0,0 +1,10 @@
+class Member
+  attr_reader :name, :role, :party, :district
+
+  def initialize(attrs = {})
+    @name = attrs["name"]
+    @role = attrs["role"]
+    @party = attrs["party"]
+    @district = attrs["district"]
+  end
+end
