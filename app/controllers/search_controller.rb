978a1e595a85d5000e41641ebc589728be8cbe4a
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-30 14:10:34 -0700

    added model and view for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,44 @@
 class SearchController < ApplicationController
 
   def index
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers['x-api-key'] = ENV["PROPUBLICA_API_KEY"]
+      faraday.adapter Faraday.default_adapter
+    end
+    response = conn.get("/congress/v1/members/house/CO/current.json")
+    @search = JSON.parse(response.body)["results"].map do |raw_member|
+      Member.new(raw_member)
+    end
 
+    # @search = MemberSearch.new
   end
 
 end
+
+class MemberSearch
+
+  attr_reader :members
+
+  def members
+    [
+      Member.new,
+      Member.new,
+      Member.new,
+      Member.new,
+      Member.new,
+      Member.new,
+      Member.new
+    ]
+  end
+end
+
+# class Member
+#   attr_reader :name, :role, :party, :district
+#
+#   def initialize(attrs = {})
+#     @name = attrs["name"]
+#     @role = attrs["role"]
+#     @party = attrs["party"]
+#     @district = attrs["district"]
+#   end
+# end
