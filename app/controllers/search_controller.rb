1dc458e3bb1b87d0643fb50dfd8b4d32dc5902ab
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-31 13:08:15 -0700

    moved logic out of controllers into POROs

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,36 +1,30 @@
 class SearchController < ApplicationController
 
   def index
-    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers['x-api-key'] = ENV["PROPUBLICA_API_KEY"]
-      faraday.adapter Faraday.default_adapter
-    end
-    response = conn.get("/congress/v1/members/house/CO/current.json")
-    @search = JSON.parse(response.body)["results"].map do |raw_member|
-      Member.new(raw_member)
-    end
+    @search = MemberSearch.new(params[:state])
+
+    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+    #   faraday.headers['x-api-key'] = ENV["PROPUBLICA_API_KEY"]
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("/congress/v1/members/house/#{params[:state]}/current.json")
+    #
+    # @search = JSON.parse(response.body, symbolize_names: true)[:results].map do |raw_member|
+    #   Member.new(raw_member)
+    # end
 
     # @search = MemberSearch.new
   end
 
 end
 
-class MemberSearch
-
-  attr_reader :members
-
-  def members
-    [
-      Member.new,
-      Member.new,
-      Member.new,
-      Member.new,
-      Member.new,
-      Member.new,
-      Member.new
-    ]
-  end
-end
+# class MemberSearch
+#
+#   attr_reader :members
+#
+#
+# end
 
 # class Member
 #   attr_reader :name, :role, :party, :district
