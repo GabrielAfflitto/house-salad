f5be2161a9125e070083a0b26e7712c66cfd3a54
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-31 13:09:36 -0700

    Changed attributes from strings to symbols to match new format

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,9 +2,9 @@ class Member
   attr_reader :name, :role, :party, :district
 
   def initialize(attrs = {})
-    @name = attrs["name"]
-    @role = attrs["role"]
-    @party = attrs["party"]
-    @district = attrs["district"]
+    @name = attrs[:name]
+    @role = attrs[:role]
+    @party = attrs[:party]
+    @district = attrs[:district]
   end
 end
